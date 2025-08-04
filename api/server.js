e8e3cf8c9866b714166217cf55f9e354c07dfbe5
Prashastii.i <prashasti433@gmail.com>
2025-08-04 19:34:43 +0530

    Initial commit

--- api/server.js
+++ api/server.js
@@ -16,8 +16,8 @@ dotenv.config();
 mongoose.set('strictQuery', true);
 const connect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO);
-    console.log('database connected');
+   await mongoose.connect(process.env.MONGO_URL); 
+   console.log('database connected');
   } catch (error) {
     console.log(error);
   }
